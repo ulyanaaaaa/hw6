0db0b88cb9a4de48e7dd0e595ce2307972d9ebd1
Ulya <user@MacBook-Pro-Ulya.local>
2023-07-10 23:30:45 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿int [] InputSomeNums()
+{
+    Console.Write("Enter count of numbers: ");
+    int size = int.Parse(Console.ReadLine()!);
+    int [] array = new int[size];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Enter {i + 1} number: ");
+        array[i] = int.Parse(Console.ReadLine()!);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+        Console.Write(", ");
+        else Console.Write(". ");
+    }
+}
+
+int CountPluses(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count = count + 1;
+        }
+    }
+    return count; 
+}
+
+
+int[] array = InputSomeNums();
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine($"Count of positives numbers = {CountPluses(array)}");
